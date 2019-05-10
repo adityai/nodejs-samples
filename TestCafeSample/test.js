e95a1f70f443a54824c1b4a32d8bd32b62b5d364
Inapurapu, Aditya <aditya.inapurapu@blueshieldca.com>
2019-05-10 08:58:48 -0700

    Initial commit

---
+++ TestCafeSample/test.js
@@ -0,0 +1,15 @@
+import { Selector } from 'testcafe'; // first import testcafe selectors
+
+fixture `Getting Started`// declare the fixture
+    .page `https://www.google.com`;  // specify the start page
+
+
+//then create a test and place your code there
+test('My first test', async t => {
+    await t
+        .typeText('#lst-ib', 'Aditya Inapurapu')
+        .pressKey('enter')
+
+        // Use the assertion to check if the actual header text is equal to the expected one
+        .expect(Selector('#rso > div:first-child > div.srg > div:first-child > div > div.rc > h3.r > a').innerText).eql('Aditya Inapurapu - Senior Automation / Release Engineer - Blue ...');
+});
